a34a7b72edb6e18468b7d0872ca684341e0f4e4f
Herbert D Ludowieg <hludotero@gmail.com>
2023-12-15 11:03:49 -0600

    Changing the way that we are making the website
    Changing from using jekyll pages to sphinx compiled
    documentation pages. I believe that this will make it
    much more visually appealing and the code will look a
    lot neater on the page. Also, makes it a lot easier
    to compile the pages and see what the site will look
    like.
    
    Currently reproduced the FD method page and some of
    the contents on the main page. Will use a mixture of
    Markdown and reStructuredTxt files. All code will be
    either in a jupyter notebook to be converted into
    Markdown with nbsphinx or straight rst.

---
+++ source/coding.rst
@@ -0,0 +1,45 @@
+Coding Projects
+===============
+Vibrav
+------
+
+Repository: https://github.com/herbertludowieg/vibrav
+
+Documentation: https://herbertludowieg.github.io/vibrav
+
+Overview
+^^^^^^^^
+
+A robust library for scientists that are looking to perform calculations
+of vibronic effects in molecules via a central finitie difference approach.
+
+ML-projects
+-----------
+
+Repository: https://github.com/herbertludowieg/ml-projects
+
+Documentation: https://herbertludowieg.github.io/ml-projects
+
+Overview
+^^^^^^^^
+
+A small collection of personal machine learning projects aimed to be more
+of a tutorial using common practices in the field of data analytics.
+
+Current Implementations
+^^^^^^^^^^^^^^^^^^^^^^^
+
+#. Linear regression analysis on the housing data set from Ames, Iowa.
+
+    - Performed data preprocessing on the data set from
+      `Kaggle <https://www.kaggle.com/c/house-prices-advanced-regression-techniques>`_
+    - Used a collection of linear regression models found in
+      `scikit-learn <https://scikit-learn.org/stable/modules/linear_model.html>`_
+      
+        - `Ordinary Least Squares <https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html>`_
+        - `Ridge regression with cross-validation <https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.RidgeCV.html>`_
+        - `LASSO with cross-validation <https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LassoCV.html>`_
+        - `Elastic Net with cross-validation <https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.ElasticNetCV.html>`_
+        
+#. Logistic Regression (Coming Soon)
+
---
+++ source/conf.py
@@ -0,0 +1,65 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Personal Website'
+copyright = '2023, Herbert Daniel Ludowieg'
+author = 'Herbert Daniel Ludowieg'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.mathjax',
+    'myst_parser',
+    'sphinx_design',
+    'nbsphinx',
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+]
+myst_dmath_allow_labels=True
+
+source_suffix = ['.rst', '.md']
+
+master_doc = 'index'
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_book_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_theme_options = {
+    "home_page_in_toc": True,
+    "show_navbar_depth": 3,
+}
+
+html_title = "Herbert D. Ludowieg"
+
+html_logo = "assets/img/phd-with-bull-crop.jpg"
+
+#togglebutton_hint = "Show More"
+#togglebutton_hint_hide = "Show Less"
